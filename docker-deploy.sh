d17a01992c910dc0895c6d94ded6ed3ea19b870c
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2024-04-06 17:07:47 +0200

    add npm build in docker container

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,2 +1,4 @@
+docker stop nicolas-guillot.fr
+docker rm nicolas-guillot.fr
 docker build -t nicolas-guillot.fr-img .
-docker run -d --rm -p 80:3000 --name nicolas-guillot.fr nicolas-guillot.fr-img
+docker run -d -p 80:3000 --name nicolas-guillot.fr nicolas-guillot.fr-img
