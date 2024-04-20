384e1494d6e9047344018c7997b50b4f5a03f11b
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2024-04-20 16:53:06 +0200

    docker-deploy.sh error fix

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,4 @@
 docker stop nicolas-guillot.fr
 docker rm nicolas-guillot.fr
 docker build -t nicolas-guillot.fr-img .
-docker run -d -p 3000:3000 --network proxy_netxork --name nicolas-guillot.fr nicolas-guillot.fr-img
+docker run -d -p 3000:3000 --network proxy_network --name nicolas-guillot.fr nicolas-guillot.fr-img
